bfe23cf584928baa48538a9005f8948ed0aa6a8f
Aaron Krishnan <buzzkri@gmail.com>
2019-05-09 11:32:03 -0700

    This is my message

--- main.rb
+++ main.rb
@@ -1,23 +1,29 @@
+require "colorize"
+
+
 def puts_git(cmd)
   puts "git #{cmd} -h"
 end
 
 def menu
-  puts "1: Enter git command"
-  puts "2: Exit"
+  puts "MAIN MENU".colorize(:cyan)
+  puts "1: Enter git command".colorize(:yellow)
+  puts "2: Exit".colorize(:yellow)
   choice = gets.to_i
   # sleep(3)
   # print "clear"
   case choice
   when 1
-    puts "Enter git command"
+    puts "Enter git command".colorize(:green)
     puts_git(gets.strip)
     menu
   when 2
-    puts "Thanks for using our program!"
+    puts "Thanks for using our program!".colorize(:blue)
     exit
   else
-    puts "Invalid Input"
+    puts "Invalid Input".colorize(:red)
+    sleep(0.5)
+    # print 'clear'       This isn't working
     menu  
   end
 end
